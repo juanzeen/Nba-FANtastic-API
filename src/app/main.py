2589378a6fe79e306a2db5a00f95edb3f172733c
juanzeen <jziignerdev@gmail.com>
2026-08-29 17:34:23 -0300

    chore: improved docs for health check and adds schemas for actual nba players

--- src/app/main.py
+++ src/app/main.py
@@ -9,7 +9,41 @@ app.include_router(historical_records.router)
 app.include_router(nba_players.router)
 
 
-@app.get("/", status_code=200, tags=["App"])
+@app.get(
+    "/",
+    status_code=200,
+    tags=["App"],
+    responses={
+        200: {
+            "description": "API is healthy",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "status": "healthy",
+                        "services": {"api": "healthy", "database": "healthy"},
+                    }
+                }
+            },
+        },
+        503: {
+            "description": "Service Unavailable / Database unreachable",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": {
+                            "status": "unhealthy",
+                            "services": {
+                                "api": "healthy",
+                                "database": "unhealthy",
+                                "error": "Connection timed out",
+                            },
+                        }
+                    }
+                }
+            },
+        },
+    },
+)
 async def health_check(client=Depends(get_client)):
     health_status = {
         "status": "healthy",
--- src/app/schemas/players.py
+++ src/app/schemas/players.py
@@ -1,36 +1,41 @@
 from typing import TypedDict, Annotated
 from pydantic import BaseModel, StringConstraints
 
+
 class TotalsStats(TypedDict):
-  games: int
-  points: int
-  assists: int
-  rebounds: int
-  blocks: int
-  steals: int
+    games: int
+    points: int
+    assists: int
+    rebounds: int
+    blocks: int
+    steals: int
+
 
 class AvgStats(TypedDict):
-      points: float
-      assists: float
-      rebounds: float
-      blocks: float
-      steals: float
+    points: float
+    assists: float
+    rebounds: float
+    blocks: float
+    steals: float
+
 
 class Team(TypedDict):
-  abbreviation: Annotated[str, StringConstraints(max_length=3)]
-  name: str
+    abbreviation: Annotated[str, StringConstraints(max_length=3)]
+    name: str
+
 
 class TotalsAvg(TypedDict):
-   totals: TotalsStats
-   avg: AvgStats
+    totals: TotalsStats
+    avg: AvgStats
+
 
 class Player(BaseModel):
-  _id: int
-  full_name: str
-  height: int
-  weight: float
-  position: str
-  country: str
-  career: TotalsAvg
-  season: TotalsAvg
-  team: Team
+    _id: int
+    full_name: str
+    height: int
+    weight: float
+    position: str
+    country: str
+    career: TotalsAvg
+    season: TotalsAvg
+    team: Team
